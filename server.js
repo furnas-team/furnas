8e185a0f0c8d6591067091cbdd82bf65fa84bb0b
Sergey <sdaleshin@gmail.com>
2018-01-21 23:09:08 +0300

    Coming soon page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/public/index.html')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
