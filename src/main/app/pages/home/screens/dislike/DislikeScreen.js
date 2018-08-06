4c3ab219e434eb110f12f70c92564fe21e8e8f5f
Sergey <sdaleshin@gmail.com>
2018-08-06 22:06:31 +0300

    added franklin and scrooge steps

--- src/main/app/pages/home/screens/dislike/DislikeScreen.js
+++ src/main/app/pages/home/screens/dislike/DislikeScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {SectionTitle} from '../../../../components/screen-title/ScreenTitle';
+import {BlockText} from '../../../../components/block-text/BlockText';
+import './dislike-screen.scss';
 
 export class DislikeScreen extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="dislike-screen">
+        <div className="dislike-screen__text">
+          <SectionTitle className="dislike-screen__title">
+            А если не понравится?
+          </SectionTitle>
+          <BlockText>
+            Результат каждого этапа обсуждаем.
+          </BlockText>
+          <BlockText>
+            Если работа не нравится, то за неё
+          </BlockText>
+          <BlockText>
+            можно не платить.
+          </BlockText>
+        </div>
+        <div className="dislike-screen__image"></div>
       </div>
     );    
   }
--- src/main/app/pages/home/screens/money/MoneyScreen.js
+++ src/main/app/pages/home/screens/money/MoneyScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import './money-screen.scss';
+import {SectionTitle} from '../../../../components/screen-title/ScreenTitle';
+import {BlockText} from '../../../../components/block-text/BlockText';
 
 export class MoneyScreen extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="money-screen">
+        <div className="money-screen__text">
+          <SectionTitle className="money-screen__title">
+            А кто делает?
+          </SectionTitle>
+          <BlockText>
+            Каждый этап оплачивается отдельно.
+          </BlockText>
+          <BlockText>
+            Всё зависит от сложности лендинга,
+          </BlockText>
+          <BlockText>
+            но в среднем один лендинг стоит
+          </BlockText>
+          <BlockText>
+            50-60 тыс. руб.
+          </BlockText>
+        </div>
+        <div className="money-screen__image"></div>
       </div>
     );    
   }
