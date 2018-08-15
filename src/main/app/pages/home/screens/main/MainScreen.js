635f36b4b2a46efa01f242a0c6300a229a2465f6
Sergey <sdaleshin@gmail.com>
2018-08-15 13:16:57 +0300

    added animation

--- src/main/app/pages/home/screens/main/MainScreen.js
+++ src/main/app/pages/home/screens/main/MainScreen.js
@@ -5,6 +5,8 @@ import {Button, ButtonStyle} from '../../../../components/button/Button';
 import {Input} from '../../../../components/input/Input';
 import './main-screen.scss';
 import classNames from 'classnames';
+import {firstScreenAnimation} from './animation/first-screen';
+import {init} from './animation/animation';
 
 export class MainScreen extends React.Component {
 
@@ -13,13 +15,21 @@ export class MainScreen extends React.Component {
   };
 
   componentDidMount() {
-    this.setState({animationFinished: true});
+    const AdobeAn = window.AdobeAn = {};
+    firstScreenAnimation(window.createjs, AdobeAn);
+    init();
+    setTimeout(() => this.setState({animationFinished: true}), 8000)
   }
 
   render() {
     const {animationFinished} = this.state;
     return (
       <div className={classNames("main-screen", {"main-screen_animation-finished": animationFinished})}>
+        <div id="animation_container">
+          <canvas id="canvas" width="320" height="560"></canvas>
+          <div id="dom_overlay_container">
+          </div>
+        </div>
         <div className="main-screen__form">
           <SectionTitle className="main-screen__title">
             <span className="main-screen__pink-word">Дизайн</span> и <span className="main-screen__blue-word">вёрстка</span> лендинга в 4 шага
