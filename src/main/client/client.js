6f863237ce2e31361ac9ac19f5f10af57d434858
Sergey <sdaleshin@gmail.com>
2018-11-19 22:08:17 +0300

    Включил mixpanel

--- src/main/client/client.js
+++ src/main/client/client.js
@@ -3,17 +3,10 @@ import {hydrate} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {App} from '../app/app';
 import './client.scss';
-// import mixpanel from 'mixpanel-browser';
+import mixpanel from 'mixpanel-browser';
 
-//mixpanel
-// window.mixpanel = mixpanel;
-// mixpanel.init('89c5c0bb6af5172d87b01e9e06839b2e');
-
-window.mixpanel = {
-  track(event){
-    console.log(event);
-  }
-};
+window.mixpanel = mixpanel;
+mixpanel.init('89c5c0bb6af5172d87b01e9e06839b2e');
 
 //yandex metrika
 window.yaCounter = new Ya.Metrika({id: 48461990});
