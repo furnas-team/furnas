7df78b8c747dbe43e2c6f8c3363aebcbb2f5a42d
Sergey <sdaleshin@gmail.com>
2019-02-27 01:31:50 +0300

    fix for safari

--- src/main/app/pages/portfolio/components/next-portfolio-screen/NextPortfolioScreen.js
+++ src/main/app/pages/portfolio/components/next-portfolio-screen/NextPortfolioScreen.js
@@ -11,7 +11,7 @@ export class NextPortfolioScreenComponent extends React.Component {
   };
 
   handleScroll = event => {
-    if ((document.documentElement.scrollTop + window.innerHeight) === document.documentElement.offsetHeight) {
+    if ((window.scrollY + window.innerHeight) === document.documentElement.offsetHeight) {
       this.props.history.push(this.props.nextPageHref);
     }
   };
