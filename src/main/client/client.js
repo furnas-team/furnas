a18d4013cae3f305d68adb975803d777a6b40a35
Sergey <sdaleshin@gmail.com>
2019-07-29 15:34:00 +0300

    added facebook pixel

--- src/main/client/client.js
+++ src/main/client/client.js
@@ -11,24 +11,50 @@ window.mixpanel = mixpanel;
 mixpanel.init('89c5c0bb6af5172d87b01e9e06839b2e');
 
 //yandex metrika
-if(typeof Ya !== 'undefined') {
+if (typeof Ya !== 'undefined') {
   window.yaCounter = new Ya.Metrika({
     id: 48461990,
-    clickmap:true,
-    trackLinks:true,
-    accurateTrackBounce:true,
-    webvisor:true
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
   });
 }
 
 //mouseflow
 window._mfq = window._mfq || [];
-(function() {
+(function () {
   var mf = document.createElement("script");
-  mf.type = "text/javascript"; mf.async = true;
+  mf.type = "text/javascript";
+  mf.async = true;
   mf.src = "//cdn.mouseflow.com/projects/355b66e5-d2a4-4c4b-8a5e-a0f778ba3dae.js";
   document.getElementsByTagName("head")[0].appendChild(mf);
 })();
+
+try {
+  !function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+      n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = !0;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = !0;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+  }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '921776094824729');
+  fbq('track', 'PageView');
+} catch {
+  
+}
 
 hydrate((
   <BrowserRouter>
