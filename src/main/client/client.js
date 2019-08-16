a7001bba52473151ad0a7d5dca0cf11345fcee87
Sergey <sdaleshin@gmail.com>
2019-08-16 19:13:18 +0300

    Added vk pixel

--- src/main/client/client.js
+++ src/main/client/client.js
@@ -53,7 +53,18 @@ try {
   fbq('init', '921776094824729');
   fbq('track', 'PageView');
 } catch (e) {
-  
+
+}
+
+try {
+  !function () {
+    var t = document.createElement("script");
+    t.type = "text/javascript", t.async = !0, t.src = "https://vk.com/js/api/openapi.js?162", t.onload = function () {
+      VK.Retargeting.Init("VK-RTRG-396159-gkOXc"), VK.Retargeting.Hit()
+    }, document.head.appendChild(t)
+  }();
+} catch (e) {
+
 }
 
 hydrate((
